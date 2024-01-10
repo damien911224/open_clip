1217143b6ca8a35ac8e79b6302e5c6b14e919c4e
damien <damien911224@gmail.com>
2024-01-10 21:14:16 +0900

    dataset debug

--- src/training/data.py
+++ src/training/data.py
@@ -77,8 +77,6 @@ class CsvVideoDataset(Dataset):
         frame_length = len(vr)
         frames = vr.get_batch(np.linspace(0, frame_length - 1, self.max_seq_len, dtype=np.int32)).permute(3, 0, 1, 2)
         images = self.transforms(frames)
-        print(images.shape)
-        exit()
         texts = self.tokenize([str(self.captions[idx])])[0]
         return images, texts
 
