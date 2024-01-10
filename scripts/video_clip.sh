134165203241ad0f9053bb0759d00344f98bb591
damien <damien911224@gmail.com>
2024-01-10 22:24:23 +0900

    dataset debug

--- scripts/video_clip.sh
+++ scripts/video_clip.sh
@@ -23,4 +23,6 @@ torchrun --nproc_per_node=4 \
     --batch-size 32 \
     --max-seq-len 16 \
     --precision amp \
-    --workers 16
+    --workers 16 \
+    --local-loss \
+    --gather-with-grad
