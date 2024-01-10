b983754121e9339dcc65fe0344fd5b5797e0c3ac
damien <damien911224@gmail.com>
2024-01-10 22:13:36 +0900

    dataset debug

--- src/training/data.py
+++ src/training/data.py
@@ -68,6 +68,8 @@ class CsvVideoDataset(Dataset):
         df.drop(index=to_be_removed)
 
         self.videos = df[img_key].tolist()
+        print(len(self.videos))
+        exit()
         self.captions = df[caption_key].tolist()
         self.transforms = transforms
         logging.debug('Done loading data.')
