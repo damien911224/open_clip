0cc905db2e4d4274b0fc54f480ad4fba7d7194d1
damien <damien911224@gmail.com>
2024-01-10 21:17:01 +0900

    dataset debug

--- src/open_clip/model.py
+++ src/open_clip/model.py
@@ -482,8 +482,6 @@ class VideoCLIP(nn.Module):
             video: Optional[torch.Tensor] = None,
             text: Optional[torch.Tensor] = None,
     ):
-        print(video.shape)
-        exit()
         N, C, T, H, W = video.shape
         video = video.transpose(1, 2).view(N * T, C, H, W)
         image_features = self.encode_image(video, normalize=False) if video is not None else None
